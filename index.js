9ee5c795b9e988928a7e20c143276755f3b2eada
Sibiraj PR <sibiraj@lightrains.com>
2020-07-23 09:15:50 +0530

    Adding index file

---
+++ index.js
@@ -0,0 +1,19 @@
+const stateMachinePlugin = require('@rentspree/mongoose-state-machine').default
+const mongoose = require('mongoose')
+const stateMachine = require('./src/stateMachine.js')
+const TelegramBot = require('./src/telegramBot.js')
+
+require('./mongodb.js')()
+
+const paymentSchema = new mongoose.Schema({
+    user: String,
+    id: String,
+    amount: String,
+    status: String
+})
+
+paymentSchema.plugin(stateMachinePlugin, { stateMachine: stateMachine })
+const Payment = mongoose.model('Payment', paymentSchema)
+
+const userId = '5f1873e0f353531f8a36bd50'
+TelegramBot(Payment, userId)
